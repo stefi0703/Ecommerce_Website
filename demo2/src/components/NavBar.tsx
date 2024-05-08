c2f2fb68b7c21da12a0d2cfd8d98dae9c8aef36b
Stefania Vintilescu <stefania.vintilescu@yahoo.com>
2024-05-08 21:51:28 +0300

    some css

--- demo2/src/components/NavBar.tsx
+++ demo2/src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./NavBar.css";
 import hackademyLogo from "./logo-color.png";
-import favoriteIcon from "./favorite.png"; // Placeholder path for favorite icon
 import helpIcon from "./help.png"; // Placeholder path for help icon
 import Cart from "./Cart"; // Import Cart component
 import Favorite from "./Favorite"; // Import Favorite component
@@ -15,34 +14,45 @@ const NavBar: React.FC<NavBarProps> = ({ cartCount, favoriteCount }) => {
   const [search, setSearch] = useState("");
 
   return (
-    <nav className="navbar">
-      <a href="/" className="navbar-brand">
-        <img src={hackademyLogo} alt="HACKADEMY" />
-      </a>
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Searching anything?"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </div>
-      <div className="navbar-icons">
-        <a href="/favorites" className="navbar-icon-link cart0">
-          <span className="icon-text">Favorite</span>
-          <Favorite favoriteCount={favoriteCount} />
+    <>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">
+          <img src={hackademyLogo} alt="HACKADEMY" />
         </a>
-        <a href="/help" className="navbar-icon-link cart0">
-          <span className="icon-text">Help</span>
-          <img src={helpIcon} alt="Help" />
-        </a>
-        <a href="/cart" className="navbar-icon-link cart0">
-          <span className="icon-text">Cart</span>
-          <Cart cartCount={cartCount} />
-        </a>
-      </div>
-    </nav>
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Searching anything?"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+        <div className="navbar-icons">
+          <a href="/favorites" className="navbar-icon-link cart0">
+            <span className="icon-text">Favorite</span>
+            <Favorite favoriteCount={favoriteCount} />
+          </a>
+          <a href="/help" className="navbar-icon-link cart0">
+            <span className="icon-text">Help</span>
+            <img src={helpIcon} alt="Help" />
+          </a>
+          <a href="/cart" className="navbar-icon-link cart0">
+            <span className="icon-text">Cart</span>
+            <Cart cartCount={cartCount} />
+          </a>
+        </div>
+      </nav>
+      <nav className="sub-navbar">
+        <a href="#!">Produse</a>
+        <a href="#!">Genius Deals</a>
+        <a href="#!">Genius</a>
+        <a href="#!">Rabla</a>
+        <a href="#!">Cardul cu milioane de idei</a>
+        <a href="#!">Resigilate</a>
+        <a href="#!">Ofertele eMAG</a>
+      </nav>
+    </>
   );
 };
 
